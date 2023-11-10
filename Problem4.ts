fee592a0827f9c1d649bfc90e7b918711207dab6
IrsanJam <irsandjamaludin@gmail.com>
2023-11-10 21:32:40 +0700

    menghilangkan any di tipe data semua function

--- Problem4.ts
+++ Problem4.ts
@@ -3,5 +3,5 @@
 //  dan tingginya berukuran tinggi cm. Ia ingin menghitung luas dari segitiga tersebut,
 //  bantulah Andi untuk menghitung luas segitiga!
 
-const luasSegitiga = (a: number, t: number): any => (1 / 2) * a * t + "cm";
+const luasSegitiga = (a: number, t: number): number | string => (1 / 2) * a * t + "cm";
 console.log(luasSegitiga(5, 7));
--- Problem5.ts
+++ Problem5.ts
@@ -1,7 +1,7 @@
 // Problem 5 - Menghitung Luas Permukaan Tabung
 // Luas permukaan tabung dapat dikatakan sebagai luas dari jumlah sisi yang dimiliki tabung.
 
-const luasPermukaanTabung = (r: number, T: number): any => 2 * 3.14 * r * (r + T) + "cm";
+const luasPermukaanTabung = (r: number, T: number): number | string => 2 * 3.14 * r * (r + T) + "cm";
 console.log(luasPermukaanTabung(4, 20));
 
 // 2vr(r + t)
